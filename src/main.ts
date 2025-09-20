3b67eaf7cd6bc0e257aa4dfe2888958797bc32a6
sunghwan <psh@dannect.com>
2025-09-21 03:20:18 +0900

    21

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { getPort } from 'get-port-please';
 
 async function bootstrap() {
+  const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
-  const port = await getPort({ portRange: [3000, 3001] });
-  await app.listen(port, async (...args) => {
-    console.log(`Server is running on ${await app.getUrl()}`);
-  });
+  await app.listen(PORT, () =>
+    console.log(`Server is running on port ${PORT}`),
+  );
 }
 bootstrap();
