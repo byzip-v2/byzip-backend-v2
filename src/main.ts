2ce5a4457320247d546c640669e999648881ceec
sunghwan <psh@dannect.com>
2025-09-23 21:15:57 +0900

    배포 테스트

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('ByZip API')
-    .setDescription('ByZip API description')
-    .setVersion('2.0.0')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  // const config = new DocumentBuilder()
+  //   .setTitle('ByZip API')
+  //   .setDescription('ByZip API description')
+  //   .setVersion('2.0.0')
+  //   .build();
+  // const documentFactory = () => SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(PORT, () =>
     console.log(`Server is running on port ${PORT}`),
