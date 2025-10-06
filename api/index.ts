8992f9ebae0226faf709e486aff0de64ef082e08
sunghwan <psh@dannect.com>
2025-10-06 15:07:17 +0900

    [FIX] Output Directory 설정 문제 해결

--- api/index.ts
+++ api/index.ts
@@ -5,14 +5,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import express from 'express';
 import { AppModule } from '../src/app.module';
 
-const server = express();
-let app: INestApplication | null = null;
+const expressServer = express();
+let app: INestApplication;
 
-async function createNestApp(): Promise<INestApplication> {
+async function createNestServer(): Promise<express.Express> {
   if (!app) {
-    app = await NestFactory.create(AppModule, new ExpressAdapter(server), {
-      logger: ['error', 'warn'],
-    });
+    app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressServer),
+      {
+        logger: ['error', 'warn', 'log'],
+      },
+    );
 
     // CORS 설정
     app.enableCors({
@@ -20,24 +24,22 @@ async function createNestApp(): Promise<INestApplication> {
       credentials: true,
     });
 
+    // Swagger 설정
     const config = new DocumentBuilder()
       .setTitle('ByZip API')
       .setDescription('The ByZip API description')
       .setVersion('2.0.0')
       .build();
-    const documentFactory = () => SwaggerModule.createDocument(app!, config);
-    SwaggerModule.setup('api', app, documentFactory);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
     await app.init();
   }
-  return app;
+
+  return expressServer;
 }
 
-// Vercel 서버리스 핸들러
-export default async (
-  req: express.Request,
-  res: express.Response,
-): Promise<void> => {
-  await createNestApp();
-  server(req, res);
+export default async (req: any, res: any) => {
+  const server = await createNestServer();
+  return server(req, res);
 };
